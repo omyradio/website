878a586a7d47562a21f214b1452ac3ab8c1b4189
Ole Lond <ole@lond.dk>
2021-04-11 20:50:09 +0200

    dynamic jitsi url ..

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,34 +1,36 @@
 $(function () {
-    const domain = location.protocol + "//meet." + location.hostname;
-    const options = {
-        roomName: "Omyradio",
-        width: "100%",
-        height: "100%",
-        parentNode: document.querySelector("#meet"),
-        configOverwrite: {
-            disableDeepLinking: true, //remove mobile app request
-            enableNoAudioDetection: false,
-            enableNoisyMicDetection: false,
-            startWithVideoMuted: true,
-            startWithAudioMuted: true,
-            startSilent: true,
-            p2p: {enabled: false}
-        },
-        interfaceConfigOverwrite: {
-            filmStripOnly: false,
-            TOOLBAR_BUTTONS: [
-                'camera', 'chat', 'settings',
-                'videoquality', 'filmstrip',
-                'tileview'
-            ],
-            MOBILE_APP_PROMO: false,
-            HIDE_KICK_BUTTON_FOR_GUESTS: true,
-            VERTICAL_FILMSTRIP: true,
-            DISABLE_RINGING: true,
-            DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
-            SHOW_JITSI_WATERMARK: false,
-            DEFAULT_BACKGROUND: '#211a0a'
+    $.getScript(location.protocol + "//meet." + location.hostname + "/external_api.js", () => {
+        const domain = location.protocol + "//meet." + location.hostname;
+        const options = {
+            roomName: "Omyradio",
+            width: "100%",
+            height: "100%",
+            parentNode: document.querySelector("#meet"),
+            configOverwrite: {
+                disableDeepLinking: true, //remove mobile app request
+                enableNoAudioDetection: false,
+                enableNoisyMicDetection: false,
+                startWithVideoMuted: true,
+                startWithAudioMuted: true,
+                startSilent: true,
+                p2p: {enabled: false}
+            },
+            interfaceConfigOverwrite: {
+                filmStripOnly: false,
+                TOOLBAR_BUTTONS: [
+                    'camera', 'chat', 'settings',
+                    'videoquality', 'filmstrip',
+                    'tileview'
+                ],
+                MOBILE_APP_PROMO: false,
+                HIDE_KICK_BUTTON_FOR_GUESTS: true,
+                VERTICAL_FILMSTRIP: true,
+                DISABLE_RINGING: true,
+                DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
+                SHOW_JITSI_WATERMARK: false,
+                DEFAULT_BACKGROUND: '#211a0a'
+            }
         }
-    }
-    new JitsiMeetExternalAPI(domain, options);
+        new JitsiMeetExternalAPI(domain, options);
+    });
 });
